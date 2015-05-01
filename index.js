66764a7ca455eff83667e5eb8e228f46659aedc8
1000ch <shogo.sensui@gmail.com>
2015-05-01 23:17:52 +0900

    update

--- index.js
+++ index.js
@@ -1,45 +1,54 @@
-(function(root) {
-  function arrayFill(array, value, start, end) {
-    if (!Array.isArray(array)) {
-      throw new TypeError('array is not a Array');
-    }
-
-    var length = array.length;
-    start = parseInt(start, 10) || 0;
-    end = end === undefined ? length : (parseInt(end, 10) || 0);
-
-    var i;
-    var l;
-
-    if (start < 0) {
-      i = Math.max(length + start, 0);
-    } else {
-      i = Math.min(start, length);
-    }
-
-    if (end < 0) {
-      l = Math.max(length + end, 0);
-    } else {
-      l = Math.min(end, length);
-    }
-
-    for (; i < l; i++) {
-      array[i] = value;
-    }
-
-    return array;
-  }
+(function(root, arrayFill) {
 
   if (typeof exports !== 'undefined') {
+
     if (typeof module !== 'undefined' && module.exports) {
       exports = module.exports = arrayFill;
     }
+
     exports.arrayFill = arrayFill;
+
   } else if (typeof define === 'function' && define.amd) {
+
     define([], function() {
       return arrayFill;
     });
-  } else {
-    root.arrayFill = arrayFill;
+
   }
-})(this);
+
+  Array.prototype.fill = function (value, start, end) {
+    arrayFill(this, value, start, end);
+  };
+
+})(this, function (array, value, start, end) {
+
+  if (!Array.isArray(array)) {
+    throw new TypeError('array is not a Array');
+  }
+
+  var length = array.length;
+  start = parseInt(start, 10) || 0;
+  end = end === undefined ? length : (parseInt(end, 10) || 0);
+
+  var i;
+  var l;
+
+  if (start < 0) {
+    i = Math.max(length + start, 0);
+  } else {
+    i = Math.min(start, length);
+  }
+
+  if (end < 0) {
+    l = Math.max(length + end, 0);
+  } else {
+    l = Math.min(end, length);
+  }
+
+  for (; i < l; i++) {
+    array[i] = value;
+  }
+
+  return array;
+
+});
--- readme.md
+++ readme.md
@@ -1,4 +1,35 @@
 # array-fill [![Build Status](https://travis-ci.org/1000ch/array-fill.svg?branch=master)](https://travis-ci.org/1000ch/array-fill)
-             
+
+Polyfill for Array.prototype.fill.
+
 [![testling badge](https://ci.testling.com/1000ch/array-fill.png)](https://ci.testling.com/1000ch/array-fill)
 
+## Usage
+
+Functionally:
+
+```javascript
+var fill = require('array-fill');
+
+console.log(fill([1, 2, 3], 4));
+// => [ 4, 4, 4 ]
+
+console.log(fill([1, 2, 3], 4, 1));
+// => [ 1, 4, 4 ]
+```
+
+From Array object:
+
+```javascript
+require('array-fill');
+
+var array = [1, 2, 3];
+array.fill(4, -3, -2);
+
+console.log(array);
+// => [ 4, 2, 3 ]
+```
+
+## License
+
+MIT: http://1000ch.mit-license.org
